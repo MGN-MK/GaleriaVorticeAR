4961966a46b8fee2d9a8df7e9452e1a1cfff1f21
annibal <annibalvillegas17@gmail.com>
2024-05-28 21:34:04 -0600

    main - Proyecto iniciado

---
+++ Galería Vórtice/Assets/Scripts/BaseClasses/CS_ArtInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_ArtInfo : MonoBehaviour
+{
+    public string title;
+    public Artist artist;
+    public Technique technique;
+    public Vector2[] sizes;
+    public Sprite img;
+}
---
+++ Galería Vórtice/Assets/Scripts/BaseClasses/E_ArtistTechniques.cs
@@ -0,0 +1,12 @@
+public enum Artist
+{
+    Theo_Camacho,
+    Oscar_Velasco,
+    Juan_José_Camacho,
+}
+
+public enum Technique
+{
+    Óleo_sobre_Tela,
+    Mixta_sobre_Tela,
+}
